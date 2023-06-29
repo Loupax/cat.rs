8600b0f2f24a78dc8bc9a90430eae1ced09495aa
Konstantinos Loupasakis <konstantinos.loupasakis@deliveryhero.com>
2023-06-29 12:55:03 +0200

    Wrote a simple implementation of cat in Rust

---
+++ README.md
@@ -0,0 +1,11 @@
+### Cat.rs
+
+A test implementation of the `cat` command in rust.
+
+### Why?
+
+Why not?
+
+### Is anyone reading these?
+
+Probably not.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env,
+    fs::File,
+    io::{self, BufReader, Read, Write}
+};
+
+use atty::Stream;
+
+const CHUNK: usize = 1024;
+fn main()->io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let files = &args[1..];
+    
+    if atty::is(Stream::Stdin){
+        for i in files{
+            let file = File::open(i)?;
+            let mut reader = BufReader::new(file);
+
+            output(&mut reader)?;
+        }
+        return Ok(());
+    }
+    
+    let stdin = io::stdin();
+    let mut reader = stdin.lock();
+
+    output(&mut reader)
+
+}
+
+fn output<R: Read>(reader: &mut R) -> io::Result<()>{
+    loop{
+        let mut buf = vec![0u8; CHUNK];
+        let len = reader.read(&mut buf)?;
+        if len < CHUNK {
+            buf.truncate(len);
+            io::stdout().write(&buf)?;
+            break
+        } else {
+            io::stdout().write(&buf)?;
+        }
+    }
+    Ok(())
 }
